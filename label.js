9a12b852e24625cfcffa80d19548742324479e0b
whtalx <whtalx@gmail.com>
2019-03-08 18:13:32 +0300

    fixed js animation

--- label.js
+++ label.js
@@ -1,36 +1,48 @@
 $(function() {
   var
-    $label           = $('.label'),
-    $label__element1 = $('.label__element').first(),
-    $label__element2 = $label__element1.next(),
+    $label                 = $('.label'),
+    $label__element1       = $('.label__element').first(),
+    $label__element2       = $label__element1.next(),
+    speed            = 33,
     transition       = 300,
     delay            = 3000,
     turn             = 0,
-    element1Img      = 1,
-    element2Img      = 2,
-    isSpinning       = false;
+    element1Img      = 1, //cycles between 1 and 3, odd only
+    element2Img      = 0, //cycles between 2 and 4, even only
+    isSpinning        = false;
+
+/*  start animations when objects are visible  */
+
+  $(window).scroll(function() {
+    if (isInViewport($label) && !isSpinning) spin();
+  });
+
+/*  fourth page labels animation  */
 
   $label__element1.attr('src','label__33-1.svg');
   $label__element2.attr('src','label__33-2.svg');
-
-  $(window).scroll(function() {
-    if (isInViewport() && !isSpinning) spin();
-  });
     
   function spin() {
     isSpinning = true;
+
     animate(function(angle) {
+
       $label__element2.css({
         'transform' :  'rotate(' + (angle + (180 * turn)) + 'deg)',
-        'opacity'   :  Math.abs(1 - turn / 1 - 1 / 180 * angle)
+        'opacity'   :  Math.abs(1 - turn - 1 / 180 * angle)
       });
+
       $label__element1.css({
-        'transform' :  'rotate(' + (180 + angle + (180 * turn)) + 'deg)',
-        'opacity'   :  Math.abs(1 - turn / 1 - 1 / 180 * (180 + angle))
+        'transform' :  'rotate(' + (angle + (180 * turn) - 180) + 'deg)',
+        'opacity'   :  Math.abs(turn - 1 / 180 * angle)
       });
     }, transition);
       
-    if (isInViewport()) setTimeout(spin, delay); else isSpinning = false;
+    if (isInViewport($label)) {
+      setTimeout(spin, delay);
+    } else {
+      isSpinning = false;
+    }
   }
 
   function animate(draw, duration) {
@@ -51,31 +63,41 @@ $(function() {
         requestAnimationFrame(animate);
       } else {
         changeImg();
-        turn++;
-        if (turn > 1) turn = 0;
       }
     });
   }
 
-  function isInViewport() {
-    var position = document.getElementsByClassName('label')[0].getBoundingClientRect();
-    return(
-        position.top < (window.innerHeight || document.documentElement.clientHeight) &&
-        position.bottom > (0)
+  function changeImg() {
+    if (turn == 0) {
+      element2Img += 2;
+      if (element2Img > 4) element2Img = 2;
+       $label__element2
+        .attr('src','label__' + speed +'-' + element2Img + '.svg');
+    }
+
+    if (turn == 1) {
+      element1Img += 2;
+      if (element1Img > 3) element1Img = 1;
+       $label__element1
+        .attr('src','label__' + speed +'-' + element1Img + '.svg');
+    }
+
+    turn++;
+    if (turn > 1) turn = 0;
+  }
+
+/*  true if at least one pixel of object height is in viewport  */
+  function isInViewport(object) {
+    if ( object instanceof $ ) {
+      var box = object[0].getBoundingClientRect();
+    } else {
+      var box = object.getBoundingClientRect();
+    }
+    return (
+      box.top < (window.innerHeight || document.documentElement.clientHeight) &&
+      box.bottom > (0)
     );
   };
 
-  function changeImg() {
-    if (turn == 0) {
-      element2Img++;
-      if (element2Img > 3) element2Img = 1;
-       $label__element2.attr('src','label__33-' + element2Img + '.svg');
-    }
-    if (turn == 1) {
-      element1Img++;
-      if (element1Img > 3) element1Img = 1;
-       $label__element1.attr('src','label__33-' + element1Img + '.svg');
-    }
-    
-  }
+  
 });
